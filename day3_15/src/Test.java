62edf0888eabac466b673fc5ca2513e61156d408
LiuZiQiao <1844861420@qq.com>
2020-04-09 23:12:31 +0800

    upload

---
+++ day3_15/src/Test.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] argv)
+    {
+//        System.out.println("hehe");
+////        Test t = new Test();
+////        t.test();
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(num);
+    }
+
+boolean flag = true;
+int i = 0;
+double d = 0;
+float f = 0;
+short s = 0;
+long  l = 0;
+byte b = 0;
+char c = 'a';
+String str = "abc";
+String st = new String();
+
+    public  void test()
+    {
+        System.out.print("this is other test");
+    }
+}
+
